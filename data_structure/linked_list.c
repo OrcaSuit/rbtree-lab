56df6efef35fb431983d2dbf82a958d42dca61a3
Ubuntu <ubuntu@ip-172-31-0-65.ap-northeast-2.compute.internal>
2023-11-14 04:36:46 +0000

    링크드리스트 구현시작

---
+++ data_structure/linked_list.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+typedef struct node_t {
+    int data;
+    node_t* next;
+}node_t;
+
+void print_node()
+{
+
+}
+
+void destroy()
+{
+    
+}
+
+void insert(){
+
+}
+
+
+int main(void) {
+    node_t* head = NULL;
+
+
+    return 0;
+}
+
