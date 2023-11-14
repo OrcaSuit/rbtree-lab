c581eec701caa8629671bef1e24aaa2add7769a7
Ubuntu <ubuntu@ip-172-31-0-65.ap-northeast-2.compute.internal>
2023-11-14 08:49:37 +0000

    동적할당한 데이터는 free() 필수

--- data_structure/linked_list.c
+++ data_structure/linked_list.c
@@ -10,21 +10,32 @@ void print_node(const node_t* head)
     node_t* p;
 
     p = head;
-    while( p != NULL ){
-      printf("%d", p->data);
-      p = p->next;
+    while( p != NULL){
+        printf("%d", p->data);
+        p = p->next;
     }
-    
 }
 
 void destroy(node_t* head)
 {
-    
+    node_t* p = head;
+
+    while(p != NULL){
+        node_t* next = p->next;
+        free(p);
+        p = next;
+    }
 }
 
 void insert(node_t** phead, int n)
 {
+    node_t* new_node;
 
+    new_node = malloc(sizeof(node_t));
+    new_node->data = n;
+
+    new_node->next = *phead;
+    *phead = new_node;
 }
 
 void remove(node_t** phead, int n)
@@ -34,8 +45,7 @@ void remove(node_t** phead, int n)
 
 int main(void) {
     node_t* head = NULL;
-
-    //insert(&head, 1);
+    insert(&head, 1);
 
     //print_node(head);
 
